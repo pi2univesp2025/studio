68e662d92a6fb92884501b26190a142f4700f3fd
Monje Careca <rajadaequilibrada@gmail.com>
2025-10-05 10:36:35 +0000

    make the text be above the whatsapp button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -202,8 +202,8 @@ export default function Home() {
         variant="default"
         className="fixed bottom-[90px] right-6 h-16 w-16 rounded-full shadow-lg flex flex-col items-center justify-center bg-[#25D366] hover:bg-[#25D366]/90 text-white dark:text-white"
       >
+        <span className="text-xs mb-1">Compre Aqui</span>
         <WhatsappIcon className="h-8 w-8" />
-        <span className="text-xs mt-1">Compre Aqui</span>
       </Button>
 
       <footer className="border-t py-6">
