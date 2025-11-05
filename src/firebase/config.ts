ccc357d13a6c1049c993ad132a1aae6f7b547972
Monje Careca <rajadaequilibrada@gmail.com>
2025-11-05 10:14:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7962404873-61fae",
+  "appId": "1:110543053507:web:adec5050c2270473a33876",
+  "apiKey": "AIzaSyC6iJSSLrABrx7HYSH5tdg2x8RN2jkqKdk",
+  "authDomain": "studio-7962404873-61fae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "110543053507"
+};
