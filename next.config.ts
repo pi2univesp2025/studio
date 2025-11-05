e97b85991fec62eba7c332cb4c248defb69e554c
Monje Careca <rajadaequilibrada@gmail.com>
2025-11-05 12:05:18 +0000

    add google photos images to remotePatterns.

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        port: '',
+        pathname: '/pw/**',
+      },
     ],
   },
 };
